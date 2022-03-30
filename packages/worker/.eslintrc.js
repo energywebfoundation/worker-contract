1bbecb0c70e999175297efe5ed6dbf4b47f8c408
patryk-kochanski <patryk.kochanski@gmail.com>
2022-03-30 12:35:10 +0200

    chore: remove prettier, add leocode lint

--- packages/worker/.eslintrc.js
+++ packages/worker/.eslintrc.js
@@ -1,24 +1,7 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  extends: ['@leocode/eslint-config/node'],
+  ignorePatterns: '**/client/**/*',
   parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    project: `${__dirname}/tsconfig.json`,
   },
 };
