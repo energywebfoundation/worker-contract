7d47d693749edf7fb161db668457d357fa8ff48e
JonaszJestem <jonaszwiacek@gmail.com>
2022-04-01 10:28:12 +0200

    chore: Add TODO actions

--- packages/merkle-tree/src/lib.rs
+++ packages/merkle-tree/src/lib.rs
@@ -2,6 +2,7 @@ use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 use neon::prelude::*;
 
+// TODO: Add real matching result struct
 #[derive(Hash)]
 struct MatchingResult {
     id: u32,
@@ -9,7 +10,10 @@ struct MatchingResult {
 
 
 fn hash_matching_result(mut cx: FunctionContext) -> JsResult<JsString> {
+    // TODO: Replace DefaultHasher with merkle tree construction
     let mut s = DefaultHasher::new();
+
+    // TODO: Replace stub matching result with parsing JS object from arguments
     let matching_result = MatchingResult {
         id: 5,
     };
