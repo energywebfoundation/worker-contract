bc79ac3942560c829f0161e61cb6e4f6e07c90dd
patryk-kochanski <patryk.kochanski@gmail.com>
2022-04-04 11:25:33 +0200

    fix: add register method

--- packages/worker/src/matching-data/matching-data-in-memory-adapter/matching-data-in-memory-adapter.module.ts
+++ packages/worker/src/matching-data/matching-data-in-memory-adapter/matching-data-in-memory-adapter.module.ts
@@ -2,19 +2,25 @@ import { Module } from '@nestjs/common';
 import { MatchingDataService } from '../matching-data.service';
 import { MatchingDataInMemoryService } from './matching-data-in-memory.service';
 import { MatchingDataFacade } from './matching-data.facade';
+import type { InputData } from './matching-data-in-memory.service';
 
-@Module({
-  imports: [],
-  exports: [
-    MatchingDataFacade,
-  ],
-  controllers: [],
-  providers: [
-    MatchingDataFacade,
-    {
-      provide: MatchingDataService,
-      useClass: MatchingDataInMemoryService,
-    },
-  ],
-})
-export class MatchingDataInMemoryAdapterModule {}
+@Module({})
+export class MatchingDataInMemoryAdapterModule {
+  public static register(input: InputData) {
+    return {
+      module: MatchingDataInMemoryAdapterModule,
+      imports: [],
+      exports: [
+        MatchingDataFacade,
+      ],
+      controllers: [],
+      providers: [
+        MatchingDataFacade,
+        {
+          provide: MatchingDataService,
+          useValue: new MatchingDataInMemoryService(input),
+        },
+      ],
+    };
+  }
+}
