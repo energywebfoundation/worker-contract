e0beec3855350ffecbf859bb178d1fdbe14e014a
Mateusz Koteja <mateusz.koteja@leocode.com>
2022-04-08 16:20:34 +0200

    refactor: use integers in SpreadMatcher

--- packages/example/src/algorithm.ts
+++ packages/example/src/algorithm.ts
@@ -1,12 +1,11 @@
-import { BigNumber } from '@ethersproject/bignumber';
 import { SpreadMatcher } from 'greenproof-algorithms';
 import type { MatchingAlgorithm } from 'greenproof-worker';
 
 export const matchingAlgorithm: MatchingAlgorithm = (input) => {
   const result = SpreadMatcher.spreadMatcher({
     entityGroups: [
-      input.consumptions.map(c => ({ id: c.deviceId, volume: BigNumber.from(c.volume) })),
-      input.generations.map(g => ({ id: g.deviceId, volume: BigNumber.from(g.volume) })),
+      input.consumptions.map(c => ({ id: c.deviceId, volume: c.volume })),
+      input.generations.map(g => ({ id: g.deviceId, volume: g.volume })),
     ],
     groupPriority: input.preferences.groupPriority,
   });
@@ -15,15 +14,15 @@ export const matchingAlgorithm: MatchingAlgorithm = (input) => {
     matches: result.matches.map(match => ({
       consumerId: match.entities[0].id,
       generatorId: match.entities[1].id,
-      volume: match.volume.toNumber(),
+      volume: match.volume,
     })),
     leftoverConsumptions: result.leftoverEntities[0].map(e => ({
       id: e.id,
-      volume: e.volume.toNumber(),
+      volume: e.volume,
     })),
     excessGenerations: result.leftoverEntities[1].map(e => ({
       id: e.id,
-      volume: e.volume.toNumber(),
+      volume: e.volume,
     })),
   };
 };
