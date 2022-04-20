154cab559c8ae64a81054033ff87af111bb41762
patryk-kochanski <patryk.kochanski@gmail.com>
2022-04-20 20:34:29 +0200

    chore: add example docker-compose

--- packages/overseer/src/example/example.ts
+++ packages/overseer/src/example/example.ts
@@ -1,7 +1,7 @@
 import type { OverseerConfig, BlockchainConfig, EventListeners } from '../types';
 
 export const localBlockchainConfig: BlockchainConfig = {
-  rpcHost: 'http://localhost:8545',
+  rpcHost: process.env.RPC_HOST ?? 'http://localhost:8545',
   contractAddress: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
   overseerPrivateKey: '0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba',
 };
