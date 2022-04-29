b1e3d9f88cabbce09996beb3934ec5c9b7795488
Daniel Wojno <32939785+dwojno@users.noreply.github.com>
2022-04-29 12:28:42 +0200

    feat(ddhubmock): add fake login

--- packages/ddhub-mock/src/api/api.module.ts
+++ packages/ddhub-mock/src/api/api.module.ts
@@ -3,9 +3,10 @@ import { FileModule } from '../file/file.module';
 import { TopicModule } from '../topic/topic.module';
 import { TopicController } from './topic/topic.controller';
 import { FileController } from './file/file.controller';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [TopicModule, FileModule],
-  controllers: [TopicController, FileController],
+  controllers: [TopicController, FileController, AuthController],
 })
 export class ApiModule {}
