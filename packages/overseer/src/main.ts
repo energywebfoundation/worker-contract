3cc214c39b85dff278b90d9064d4b5e18227efc3
patryk-kochanski <37582269+patryk-kochanski@users.noreply.github.com>
2022-04-29 11:24:57 +0200

    Feat/logger (#18)
    * feat: add pino logger
    
    * chore: add logs

--- packages/overseer/src/main.ts
+++ packages/overseer/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { exampleConfig } from './example/example';
 import { OverseerModule } from './overseer.module';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(OverseerModule.register(exampleConfig));
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
--- packages/worker/src/matching-result/matching-result-receivers-adapter/matching-result-receivers.facade.ts
+++ packages/worker/src/matching-result/matching-result-receivers-adapter/matching-result-receivers.facade.ts
@@ -3,9 +3,11 @@ import type { MatchingResultReceiver} from './types';
 import { MATCHING_RESULT_RECEIVERS } from './types';
 import type { MatchingResultFacade } from '../matching-result.facade';
 import type { MatchingResult } from '../types';
+import { PinoLogger } from 'nestjs-pino';
 
 @Injectable()
 export class MatchingResultReceiversFacade implements MatchingResultFacade {
+  private logger = new PinoLogger({ renameContext: MatchingResultReceiversFacade.name});
   constructor(
     @Inject(MATCHING_RESULT_RECEIVERS)
     private receivers: MatchingResultReceiver[],
@@ -14,11 +16,14 @@ export class MatchingResultReceiversFacade implements MatchingResultFacade {
   public async receiveMatchingResult(
     result: MatchingResult,
   ) {
+    this.logger.info('Receiving matching results.');
+
     for (const receiver of this.receivers) {
       // Clone result so receiver doesn't modify it accidentaly for another receiver
       const clonedResult = JSON.parse(JSON.stringify(result));
 
       await receiver(clonedResult);
     }
+    this.logger.info('Receiving of matching results complete.');
   }
 }
