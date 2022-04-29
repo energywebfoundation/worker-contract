7851ced58c4ff7c0a4cb9a3111d15424e0332dba
patryk-kochanski <37582269+patryk-kochanski@users.noreply.github.com>
2022-04-29 11:24:57 +0200

    Feat/logger (#18)
    * feat: add pino logger
    
    * chore: add logs

--- packages/overseer/src/overseer.module.ts
+++ packages/overseer/src/overseer.module.ts
@@ -2,21 +2,27 @@ import { Module } from '@nestjs/common';
 import { OverseerController } from './example/example.controller';
 import { OverseerService } from './overseer.service';
 import type { OverseerConfig } from './types';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({})
 export class OverseerModule {
+
+
   public static register(params: OverseerConfig) {
     return {
       module: OverseerModule,
-      imports: [],
+      imports: [
+        LoggerModule.forRoot(),
+      ],
       controllers: [
         OverseerController,
       ],
       providers: [
         {
           provide: OverseerService,
-          useValue: new OverseerService(params.blockchainConfig, params.listeners, params.getLastHandledBlockNumber)},
+          useValue: new OverseerService(params.blockchainConfig, params.listeners, params.getLastHandledBlockNumber),
+        },
       ],
-    }
+    };
   }
 }
