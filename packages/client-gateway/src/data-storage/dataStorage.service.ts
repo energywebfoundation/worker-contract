3aab18916d8a1bdf065f2c3b42836f244f185545
Mateusz Koteja <mateusz.koteja@leocode.com>
2022-05-13 12:28:15 +0200

    chore: move rest of packages

--- packages/client-gateway/src/data-storage/dataStorage.service.ts
+++
@@ -1,6 +0,0 @@
-import type { Reading } from '../data-storage/types';
-
-export abstract class DataStorageService {
-  abstract sendConsumptions(readings: Reading[]): Promise<void>;
-  abstract sendGenerations(readings: Reading[]): Promise<void>;
-}
