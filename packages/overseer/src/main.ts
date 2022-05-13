88e0a4b021a961f0c9460bd6a8bcb622cabe60a4
patryk-kochanski <patryk.kochanski@gmail.com>
2022-05-13 14:52:23 +0200

    chore: env

--- packages/overseer/src/main.ts
+++ packages/overseer/src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { OverseerModule } from './overseer.module';
 import { Logger } from 'nestjs-pino';
 import type { OverseerConfig } from './types';
+import { join } from 'path';
+import { config } from 'dotenv';
+config({ path: join(__dirname, '..', '.env') });
 
 const port = process.env.PORT || 3000;
 const blockchainConfig = {
