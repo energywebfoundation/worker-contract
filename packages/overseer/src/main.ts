59529f95862adc87917a221fc9d385e54fc25fbe
Mateusz Koteja <mateusz.koteja@leocode.com>
2022-05-16 15:34:26 +0200

    chore(overseer): fix export of start function to avoid side-effect

--- packages/overseer/src/main.ts
+++ packages/overseer/src/main.ts
@@ -1,9 +1,6 @@
-import { NestFactory } from '@nestjs/core';
-import { OverseerModule } from './overseer.module';
-import { Logger } from 'nestjs-pino';
-import type { OverseerConfig } from './types';
 import { join } from 'path';
 import { config } from 'dotenv';
+import { start } from '.';
 config({ path: join(__dirname, '..', '.env') });
 
 const port = process.env.PORT || 3000;
@@ -11,18 +8,10 @@ const blockchainConfig = {
   rpcHost: process.env.RPC_HOST!,
   contractAddress: process.env.CONTRACT_ADDRESS!,
   overseerPrivateKey: process.env.OVERSEER_PRIVATE_KEY!,
-
 };
 
-export async function bootstrap(config: OverseerConfig) {
-  const app = await NestFactory.create(OverseerModule.register(config));
-  app.useLogger(app.get(Logger));
-  await app.listen(port);
-  console.log(`Overseer listening on port ${port}.`);
-}
-
-bootstrap({
+start({
   blockchainConfig,
   listeners: {},
   getLastHandledBlockNumber: () => { return 0; },
-});
+}, Number(port));
