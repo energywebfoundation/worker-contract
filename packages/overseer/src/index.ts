db2494f7f0710ad19544baa6b7ba1c21462ca5c6
Mateusz Koteja <mateusz.koteja@leocode.com>
2022-05-16 15:34:26 +0200

    chore(overseer): fix export of start function to avoid side-effect

--- packages/overseer/src/index.ts
+++ packages/overseer/src/index.ts
@@ -1,2 +1,13 @@
-export { bootstrap as start } from './main';
+import { NestFactory } from '@nestjs/core';
+import { OverseerModule } from './overseer.module';
+import { Logger } from 'nestjs-pino';
+import type { OverseerConfig } from './types';
+
+export async function start(config: OverseerConfig, port: number) {
+  const app = await NestFactory.create(OverseerModule.register(config));
+  app.useLogger(app.get(Logger));
+  await app.listen(port);
+  console.log(`Overseer listening on port ${port}.`);
+}
+
 export type { OverseerConfig } from './types';
