c8205cfcbf1effda130c52acbe299ecfb5b6625b
JonaszJestem <jonaszwiacek@gmail.com>
2022-05-24 10:32:10 +0200

    feat: preserve index template

--- packages/voting-manager/substitute-vars.js
+++ packages/voting-manager/substitute-vars.js
@@ -1,4 +1,10 @@
 const { exec } = require("child_process");
 
+// Restore template index
+exec("cp ./build/index.template.html ./build/index.html ");
+
+// Preserve original index.html
 exec("cp ./build/index.html ./build/index.template.html");
+
+// Substitute variables in index.html
 exec("envsubst < ./build/index.template.html > ./build/index.html");
