2817b69916b9f14b67041361e2d077483ea96300
JonaszJestem <jonaszwiacek@gmail.com>
2022-05-24 13:30:46 +0200

    fix: cr commments

--- packages/voting-manager/substitute-vars.js
+++ packages/voting-manager/substitute-vars.js
@@ -1,10 +1,18 @@
 const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
 
-// Restore template index
-exec("cp ./build/index.template.html ./build/index.html ");
+const indexTemplatePath = path.join(__dirname, "build", "index.template.html");
+const indexPath = path.join(__dirname, "build", "index.html");
 
-// Preserve original index.html
-exec("cp ./build/index.html ./build/index.template.html");
+if (!fs.existsSync(indexTemplatePath)) {
+  // Create template from original index.html
+  fs.copyFileSync(indexPath, indexTemplatePath);
+}
+
+// Restore original template
+fs.copyFileSync(indexTemplatePath, indexPath);
+
 
 // Substitute variables in index.html
-exec("envsubst < ./build/index.template.html > ./build/index.html");
+exec(`envsubst < ${indexTemplatePath} > ${indexPath}`);
