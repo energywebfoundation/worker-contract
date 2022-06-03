3f7ab91870c5552bae31a50cfe4347fd03402a99
patryk-kochanski <37582269+patryk-kochanski@users.noreply.github.com>
2022-06-03 16:42:45 +0200

    chore(worker): remove ddhub and contract adapters (#52)

--- packages/worker/src/matching-result/matching-result-receivers-adapter/index.ts
+++ packages/worker/src/matching-result/matching-result-receivers-adapter/index.ts
@@ -1,6 +1,4 @@
 export * from './matching-result-logger.receiver';
-export * from './matching-result-ddhub.receiver';
 export * from './matching-result-receivers.facade';
 export * from './matching-result.module';
-export * from './matching-result-voting-contract.receiver';
 export * from './types';
