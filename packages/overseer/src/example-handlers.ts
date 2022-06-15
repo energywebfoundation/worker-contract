02aacd7d3150cde5f9d39c9f0487fbb5943ddbcc
Jonasz <jonaszwiacek@gmail.com>
2022-06-15 09:52:12 +0200

    fix: event handling (#57)
    * fix: event handling
    
    * fix: typo

--- packages/overseer/src/example-handlers.ts
+++ packages/overseer/src/example-handlers.ts
@@ -2,14 +2,29 @@ import type { EventListeners } from './types';
 import * as fs from 'fs';
 import { join } from 'path';
 
-// Note: leaving this as example
+/*
+Note: leaving this as example
+ */
+
+// Blockchain event arguments are stored in array indexed both by incremented numbers and strings
+// e.g. Array [ '0': 'firstArg', '1': 'secondArg', 'firstKey': 'firstArg', 'secondKey': 'secondArg' ]
+// We can filter out the number indexed keys and construct the object from the rest to parse the args.
+const parseEventArgs = (eventArguments: Record<string, string>) =>
+  Object.fromEntries(Object.entries(eventArguments).filter(
+    ([k]) => !Number.isInteger(Number.parseInt(k, 10)),
+  ));
 
 export const listeners: EventListeners = {
-  WinningMatch: [(...ev: any) => {
-    const [matchInput, matchResult, voteCount] = ev;
-    console.log({matchInput});
-    console.log({matchResult});
-    console.log({voteCount});
+  WinningMatch: [(event) => {
+    if (!event.args) {
+      console.debug(`No event arguments in event: ${event.event}`);
+      return;
+    }
+
+    const { matchInput, matchResult, voteCount } = parseEventArgs(event.args);
+    console.log({ matchInput });
+    console.log({ matchResult });
+    console.log({ voteCount });
   }],
 };
 
