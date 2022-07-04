b174d25fc6c5efda2683ad05b0d67aa2d86ad26d
Daniel Wojno <32939785+dwojno@users.noreply.github.com>
2022-07-04 16:12:34 +0200

    chore: change hashing algo to SHA256 (#65)

--- packages/merkle-tree/src/hashes.ts
+++ packages/merkle-tree/src/hashes.ts
@@ -1,6 +1,6 @@
 import {createHmac} from 'crypto';
 
-export const HASHING_FUNCTION = 'SHA512';
+export const HASHING_FUNCTION = 'SHA256';
 
 export const hash = (thingToHash: string, key: string): string => {
   const hmac = createHmac(HASHING_FUNCTION, key);
