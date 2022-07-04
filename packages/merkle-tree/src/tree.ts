41cb4a3edd265c36ae09d494f4df4c898b82a04b
Daniel Wojno <32939785+dwojno@users.noreply.github.com>
2022-07-04 16:12:34 +0200

    chore: change hashing algo to SHA256 (#65)

--- packages/merkle-tree/src/tree.ts
+++ packages/merkle-tree/src/tree.ts
@@ -1,16 +1,17 @@
-import {createHash} from 'crypto';
+import { createHash } from 'crypto';
 
-import {MerkleTree} from 'merkletreejs';
-import {HASHING_FUNCTION} from './hashes';
+import { MerkleTree } from 'merkletreejs';
+import { HASHING_FUNCTION } from './hashes';
 
-
-const treeHashFunction = (thingToHash: string) => createHash(HASHING_FUNCTION).update(thingToHash).digest('hex');
+const hash = (thingToHash: string) =>
+  createHash(HASHING_FUNCTION).update(thingToHash).digest('hex');
 
 export const createMerkleTree = (leaves: string[]) => {
-  const tree = new MerkleTree(leaves, treeHashFunction);
+  const tree = new MerkleTree(leaves, hash);
 
   return {
-    verify: (leaf: string) => tree.verify(tree.getProof(leaf), leaf, tree.getRoot()),
+    verify: (leaf: string) =>
+      tree.verify(tree.getProof(leaf), leaf, tree.getRoot()),
     tree,
   };
 };
