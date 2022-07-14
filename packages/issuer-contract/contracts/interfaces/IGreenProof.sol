c1cf29b3d20c75df9d0cc3fcc106eb43c30a83bb
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-07-14 16:48:14 +0200

    feat(issuer-contract): setting basic proof structure

---
+++ packages/issuer-contract/contracts/interfaces/IGreenProof.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+interface IGreenProof {
+    struct Proof {
+        uint256 index;
+        uint256 productType;
+        uint256 volume;
+        uint256 startTime;
+        uint256 endTime;
+        bytes32 producerRef;
+        bool isRevoked;
+    }
+
+}
