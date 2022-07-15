9b1293a58a758a9f4e3c9291b5d84feede23f637
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-07-15 17:08:34 +0200

    feat(issuer-contract): setting issuer template * adding issueProof function * adding revokeProof function structure * adding retireProof function structure setting tests boilerplate

--- packages/issuer-contract/contracts/Minter.sol
+++ packages/issuer-contract/contracts/Minter.sol
@@ -12,7 +12,11 @@ contract Issuer is Ownable, ERC1155 {
     uint256 lastProofIndex;
     
     modifier onlyMinter(){
-    //TO: set a requirement checking for authorized entity
+    //TO: set a requirement checking for authorized issuer entity
+        _;
+    }
+    modifier onlyRevoker(){
+    //TO: set a requirement checking for authorized revoker entity
         _;
     }
 
@@ -21,18 +25,48 @@ contract Issuer is Ownable, ERC1155 {
     constructor(string memory uri) ERC1155(uri) {
     }
 
-    function mint(address receiver, uint256 amount, uint256 productType, bytes memory data, uint256 start, uint256 end, bytes32 producerRef) external onlyMinter {
+    //TO-DO: precising the format of data inputs : bytes encoded or distincly provided
+    function issueProof(
+        address receiver,
+        uint256 amount,
+        uint256 productType,
+        bytes memory data,
+        uint256 start,
+        uint256 end,
+        bytes32 producerRef) external onlyMinter {
         bool isRevoked = false;
+        bool isRetired = false;
         IGreenProof.Proof memory greenProof = IGreenProof.Proof(
+            isRevoked,
+            isRetired,
             lastProofIndex,
             productType,
             amount,
             start,
             end,
-            producerRef,
-            isRevoked
+            producerRef
         );
         mintedProofs[lastProofIndex] = greenProof;
+
         ERC1155._mint(receiver, productType, amount, data);
+    }
+
+    function getProof(uint256 proofIndex) external view returns(IGreenProof.Proof memory proof){
+        proof = mintedProofs[proofIndex];
+    }
+
+    function revokeProof(uint256 proofIndex) external onlyRevoker {
+        require(mintedProofs[proofIndex].isRevoked == false, "already revoked proof");
+        //TO-DO: check that we are not allowed to revoke retired proofs 
+        require(mintedProofs[proofIndex].isRetired == false, "Not allowed on retired proofs");
+        //TO-DO: revoke the proof
+        mintedProofs[proofIndex].isRevoked = true;
+    }
+
+    function retireProof(address from, uint256 proofIndex) external {
+        require(mintedProofs[proofIndex].isRevoked == false, "proof revoked");
+        require(mintedProofs[proofIndex].isRetired == false, "Proof already retired");
+        require(_msgSender() == from || ERC1155.isApprovedForAll(from, _msgSender()), "Not allowed to retire");
+        _burn(from, mintedProofs[proofIndex].productType, mintedProofs[proofIndex].volume);
     }
 }
