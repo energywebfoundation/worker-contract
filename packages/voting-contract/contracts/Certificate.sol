5221eb779f1253b1f56a442b62c1817f0e7ed376
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-07-19 14:39:58 +0200

    fix(voting contract): updated contract to use bytes32 instead of strings

--- packages/voting-contract/contracts/Certificate.sol
+++ packages/voting-contract/contracts/Certificate.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.0;
 
 interface ICertificate {
-    function mint(string memory matchInput, string memory matchResult) external;
+    function mint(bytes32 matchInput, bytes32 matchResult) external;
 }
 
 contract Certificate {
-    mapping(string => string) public matches;
+    mapping(bytes32 => bytes32) public matches;
 
     /// Event emitted after certificate is minted
-    event CertificateMinted(string matchInput, string matchResult);
+    event CertificateMinted(bytes32 matchInput, bytes32 matchResult);
 
-    function mint(string memory matchInput, string memory matchResult)
+    function mint(bytes32 matchInput, bytes32 matchResult)
         external
     {
         matches[matchInput] = matchResult;
