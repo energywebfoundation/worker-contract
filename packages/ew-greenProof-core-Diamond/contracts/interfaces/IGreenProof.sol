c114e91748928f43be7d3377fb9c2ef4e5a45481
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-08-17 01:13:33 +0200

    feat(greenproof-issuer): implementing issuance validation functions and events

---
+++ packages/ew-greenProof-core-Diamond/contracts/interfaces/IGreenProof.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+interface IGreenProof {
+    struct Proof {
+        bool isRevoked;
+        bool isRetired;
+        uint256 index;
+        uint256 productType;
+        uint256 volume;
+        uint256 startTime;
+        uint256 endTime;
+        bytes32 producerRef;
+    }
+
+    enum RequestStatus {
+        DEFAULT,
+        PENDING,
+        REJECTED,
+        ACCEPTED
+    }
+
+    struct IssuanceRequest {
+        uint256 requestID;
+        address recipient;
+        string winningMatch;
+        bytes verifiableCredential;
+        RequestStatus status;
+    }
+
+    function issueProof(
+        address receiver,
+        uint256 amount,
+        uint256 productType,
+        uint256 start,
+        uint256 end,
+        string memory winningMatch,
+        bytes32 producerRef
+    ) external;
+
+    function retireProof(address from, uint256 proofID) external;
+    function getProof(uint256 proofID) external view returns(Proof memory proof);
+    function requestProofIssuance(string memory winningMatch, address recipient) external;
+}
