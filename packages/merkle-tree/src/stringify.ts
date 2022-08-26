2deccead91150ac2bfde53476549ba7760991467
Daniel Wojno <32939785+dwojno@users.noreply.github.com>
2022-08-26 15:18:08 +0200

    chore: simplified solution

--- packages/merkle-tree/src/stringify.ts
+++ packages/merkle-tree/src/stringify.ts
@@ -1,20 +1,35 @@
-export const stringify = (
-  target: Record<string, string | number | null | undefined>,
-  {
-    keyValueDelimiter = ':',
-    objectsDelimiter = ',',
-  }: { keyValueDelimiter?: string; objectsDelimiter?: string } = {},
-): string => {
+interface Target {
+  [k: string]: string | number | boolean | null | undefined | Target
+}
+
+const sortObject = (target: Target) => {
   const keys = Object.keys(target);
-  if (keys.length < 1) return '';
+  if (keys.length < 1) return target;
+
+  keys.sort();
+
   return keys
-    .sort()
     .reduce((acc, key) => {
       const value = target[key];
-      if (value) {
-        acc.push(`${key}${keyValueDelimiter}${value}`);
+      if (value || typeof value === 'boolean') {
+        if (
+          typeof value === 'string' ||
+          typeof value === 'number' ||
+          typeof value === 'boolean'
+        ) {
+          acc[key] = value;
+        }
+        if (typeof value === 'object') {
+          acc[key] = sortObject(value);
+        }
       }
       return acc;
-    }, [] as string[])
-    .join(objectsDelimiter);
+    }, {} as Target);
+};
+
+export const stringify = (
+  target: Target,
+): string => {
+  const sorted = sortObject(target);
+  return JSON.stringify(sorted);
 };
