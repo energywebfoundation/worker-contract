656cad4d755875dafa842b934ce91bc8fbd52113
Daniel Wojno <32939785+dwojno@users.noreply.github.com>
2022-08-29 12:34:07 +0200

    chore: update docs

--- packages/merkle-tree/src/stringify.ts
+++ packages/merkle-tree/src/stringify.ts
@@ -49,8 +49,8 @@ const sortObject = (target: Target) => {
 
 /**
  *
- * @param {object} target object containing string, number, boolean or other object containing those values.
- * @remember Other values like functions or arrays will be omitted.
+ * @param {object} target object containing string, number, boolean, arrays or other object containing those values.
+ * @remember Other values like functions will be omitted.
  * @remember Function compatible with JSON.parse
  * @returns {string} sorted and stringified object
  */
