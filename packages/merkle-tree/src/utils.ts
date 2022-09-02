df7561ae1d70721b67c0a1e9733286c483e45d15
Daniel Wojno <32939785+dwojno@users.noreply.github.com>
2022-09-02 10:01:52 +0200

    feat: add precise proofs (#85)

---
+++ packages/merkle-tree/src/utils.ts
@@ -0,0 +1,44 @@
+export const sortArray = <T>(target: NonNullable<T>[]): T[] => {
+  const preparedArray = target.map(item => {
+    if (typeof item === 'object') {
+      return JSON.stringify(sortObject(item));
+    }
+    return String(item);
+  }).sort();
+  return preparedArray.map(item => {
+    try {
+      return JSON.parse(item);
+    } catch {
+      return item;
+    }
+  });
+};
+
+export const sortObject = <T extends Record<string, any>>(target: T): T => {
+  const keys = Object.keys(target);
+  if (keys.length < 1) return target;
+
+  keys.sort();
+
+  return keys
+    .reduce((acc, key) => {
+      const value = target[key];
+      if (value || typeof value === 'boolean') {
+        if (
+          typeof value === 'string' ||
+          typeof value === 'number' ||
+          typeof value === 'boolean'
+        ) {
+          acc[key] = value;
+        }
+        if (typeof value === 'object' && !Array.isArray(value)) {
+          acc[key] = sortObject(value);
+        }
+
+        if (Array.isArray(value)) {
+          acc[key] = sortArray(value);
+        }
+      }
+      return acc;
+    }, {} as any);
+};
