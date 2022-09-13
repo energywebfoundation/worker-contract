c1316a0fcf30bec0be07d6fcd1d141670627b6a4
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-09-13 10:42:48 +0200

    feat(Testing): testing proof verification

--- packages/ew-greenProof-core-Diamond/test/utils/index.js
+++ packages/ew-greenProof-core-Diamond/test/utils/index.js
@@ -1,6 +1,6 @@
 const { utils } = require("ethers");
-const { checkProof } = require("./merkleProof.utils");
 const { claimManagerABI } = require("./claimManager_abi");
+const { checkProof, getMerkleProof} = require("./merkleProof.utils");
 
 const toBytes32 = (input) => {
   return utils.formatBytes32String(input);
@@ -11,5 +11,6 @@ const claimManagerInterface = claimManagerABI;
 module.exports = {
   toBytes32,
   checkProof,
+  getMerkleProof,
   claimManagerInterface,
 };
