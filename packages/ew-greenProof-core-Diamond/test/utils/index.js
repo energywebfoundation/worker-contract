ba178700339d5f08062fbca29520300b7e7ddae7
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2022-09-15 21:43:05 +0200

    chore(deployer): adding claimManager Mocking in utils

--- packages/ew-greenProof-core-Diamond/test/utils/index.js
+++ packages/ew-greenProof-core-Diamond/test/utils/index.js
@@ -1,16 +1,51 @@
 const { utils } = require("ethers");
 const { claimManagerABI } = require("./claimManager_abi");
-const { checkProof, getMerkleProof} = require("./merkleProof.utils");
+const { getMerkleProof} = require("./merkleProof.utils");
+const {
+  deployMockContract,
+  MockContract,
+  solidity,
+} = require("ethereum-waffle");
+
 
 const toBytes32 = (input) => {
   return utils.formatBytes32String(input);
 };
 
+const defaultVersion = 1;
 const claimManagerInterface = claimManagerABI;
+
+const initMockClaimManager = async () => {
+  [owner] = await ethers.getSigners();
+
+  //  Mocking claimManager
+    const claimManagerMocked = await deployMockContract(
+      owner,
+      claimManagerInterface
+    );
+
+    const grantRole = async (operatorWallet, role) => {
+      await claimManagerMocked.mock.hasRole
+        .withArgs(operatorWallet.address, role, defaultVersion)
+        .returns(true);
+    };
+
+    const revokeRole = async (operatorWallet, role) => {
+      await claimManagerMocked.mock.hasRole
+        .withArgs(operatorWallet.address, role, defaultVersion)
+        .returns(false);
+    };
+  
+  return {
+    revokeRole,
+    grantRole,
+    claimManagerMocked,
+  }
+}
 
 module.exports = {
   toBytes32,
-  checkProof,
   getMerkleProof,
+  initMockClaimManager,
   claimManagerInterface,
 };
