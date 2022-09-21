176f9b77174ad14f8c68c9531dbb98b5b5b37bcc
Daniel Wojno <32939785+dwojno@users.noreply.github.com>
2022-09-21 14:21:35 +0200

    chore: add ddhub-client to worker

--- packages/worker/src/main.ts
+++ packages/worker/src/main.ts
@@ -4,6 +4,8 @@ import type { MatchVoting } from '@energyweb/greenproof-voting-contract';
 import { MatchVoting__factory } from '@energyweb/greenproof-voting-contract';
 import { createMerkleTree, stringify, verify, createPreciseProof } from '@energyweb/greenproof-merkle-tree';
 import { providers, Wallet } from 'ethers';
+import type { Config } from '@energyweb/greenproof-ddhub-client';
+import { DDHubClient } from '@energyweb/greenproof-ddhub-client';
 import * as Joi from 'joi';
 
 const configSchema = Joi.object<WorkerConfig>({
@@ -20,6 +22,13 @@ type WorkerConfig = {
   port?: number;
 };
 
+type DDHUBConfig = {
+  appNamespace: string;
+  debugMode: boolean;
+  channelConfig: Config[];
+  ddhubUrl: string;
+}
+
 export type MerkleTree = {
   createMerkleTree: typeof createMerkleTree;
   stringify: typeof stringify;
@@ -30,6 +39,7 @@ export type MerkleTree = {
 type Runtime = {
   merkleTree: MerkleTree;
   getVotingContract: () => MatchVoting;
+  ddhubClient: DDHubClient,
 };
 
 type CallBack = (runtime: Runtime) => Promise<void>;
@@ -40,6 +50,7 @@ export class GreenProofWorker {
   private votingContractAddress: string;
   merkleTree: MerkleTree;
   port: number;
+  _ddhubClient: DDHubClient | null = null;
 
   constructor(config: WorkerConfig) {
     const { privateKey, rpcUrl, votingContractAddress, port } = this.validateConfig(config);
@@ -49,6 +60,24 @@ export class GreenProofWorker {
 
     this.merkleTree = { createMerkleTree, stringify, verify, createPreciseProof };
     this.port = port ?? 3030;
+  }
+
+  public get ddhubClient() {
+    if (!this._ddhubClient) {
+      throw new Error('DDHub communication not enabled. Please use "enableDDHubCommunication" method!');
+    }
+    return this._ddhubClient;
+  }
+
+  public async enableDDHubCommunication({appNamespace, channelConfig, debugMode, ddhubUrl}: DDHUBConfig) {
+    this._ddhubClient = new DDHubClient({
+      config: channelConfig,
+      ddhubUrl,
+      ownerNamespace: appNamespace,
+      privateKey: this.privateKey,
+      debugModeOn: debugMode ?? false,
+    });
+    await this._ddhubClient.setup();
   }
 
   private validateConfig(config: WorkerConfig) {
@@ -70,14 +99,24 @@ export class GreenProofWorker {
       this.provider.getSigner(),
     );
     return contract.connect(signer);
+  };
+
+  private async registerWorkerAsAVoter() {
+    const workerAddress = this.getContractWithSigner().address;
+    const isWorker = await this.getContractWithSigner().isWorker(workerAddress);
+    if (isWorker) return;
+    const tx = await this.getContractWithSigner().addWorker(workerAddress);
+    await tx.wait();
   }
 
   async start(cb: CallBack) {
     const app = await NestFactory.create(AppModule);
     await app.listen(this.port);
+    await this.registerWorkerAsAVoter();
     await cb({
       merkleTree: this.merkleTree,
       getVotingContract: this.getContractWithSigner,
+      ddhubClient: this.ddhubClient,
     });
   }
 }
