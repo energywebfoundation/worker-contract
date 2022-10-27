d5285bbad6c8e2520f67d24f8efb2818eda963c5
Jonasz <jonaszwiacek@gmail.com>
2022-10-27 16:37:21 +0200

    refactor: create build script in typescript

--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -1,5 +1,4 @@
 export * from './main';
-export { VotingFacet as VotingContract } from '@energyweb/greenproof-contracts';
 export * as merkleTree from '@energyweb/greenproof-merkle-tree';
 export * as ddhubClient from '@energyweb/greenproof-ddhub-client';
 export * as contracts from '@energyweb/greenproof-contracts';
