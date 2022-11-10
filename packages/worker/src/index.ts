7f2587315b56feb05419c886bdea097239482d19
Jonasz <jonaszwiacek@gmail.com>
2022-11-10 15:48:02 +0100

    fix: deployment

--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -2,4 +2,4 @@ export * from './main';
 export * as merkleTree from '@energyweb/greenproof-merkle-tree';
 export * as ddhubClient from '@energyweb/greenproof-ddhub-client';
 export * as contracts from '@energyweb/greenproof-contracts';
-export type { VotingFacet, IssuerFacet, ERC1155Base } from '@energyweb/greenproof-contracts';
+export type { VotingFacet, IssuerFacet, Diamond, ERC1155Base } from '@energyweb/greenproof-contracts';
