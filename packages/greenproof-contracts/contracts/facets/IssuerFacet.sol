afef283276e90d42bd8de79093a5a9a9c06e8d84
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2023-01-12 22:11:35 +0100

    chore(Greenproof-contracts): removing redundant imports

--- packages/greenproof-contracts/contracts/facets/IssuerFacet.sol
+++ packages/greenproof-contracts/contracts/facets/IssuerFacet.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
-import {IVoting} from "../interfaces/IVoting.sol";
 import {LibIssuer} from "../libraries/LibIssuer.sol";
 import {LibVoting} from "../libraries/LibVoting.sol";
 import {IGreenProof} from "../interfaces/IGreenProof.sol";
