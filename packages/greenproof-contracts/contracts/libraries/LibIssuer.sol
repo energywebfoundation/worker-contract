52ba30e72f12a62e2196fc7b7b83131f2d594f28
Jonasz <jonaszwiacek@gmail.com>
2023-01-13 12:18:58 +0100

    fix: gp-691 Implement Audit feedback- L13- Duplicated Event Declarations

--- packages/greenproof-contracts/contracts/libraries/LibIssuer.sol
+++ packages/greenproof-contracts/contracts/libraries/LibIssuer.sol
@@ -20,6 +20,7 @@ library LibIssuer {
         mapping(bytes32 => mapping(string => string)) disclosedData;
         //checks that data is disclosed for a specific key (string) of a precise certificate (bytes32)
         mapping(bytes32 => mapping(string => bool)) isDataDisclosed;
+        // @notice saving the data needed for future features
         mapping(bytes32 => mapping(bytes32 => uint256)) voteToCertificates;
     }
 
