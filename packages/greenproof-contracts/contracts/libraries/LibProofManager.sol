953d477804b027e63f48624c9dea77233eee4d31
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2023-01-23 18:41:36 +0100

    chore(ProofManager): optimizing claiming verification flow

--- packages/greenproof-contracts/contracts/libraries/LibProofManager.sol
+++ packages/greenproof-contracts/contracts/libraries/LibProofManager.sol
@@ -11,28 +11,45 @@ library LibProofManager {
     error NonExistingCertificate(uint256 certificateID);
     error NonRevokableProof(uint256 certificateID, uint256 issuanceDate, uint256 revocableDateLimit);
 
-    function checkNotRevokedProof(uint256 certificateID) internal view {
-        LibIssuer.IssuerStorage storage issuer = LibIssuer._getStorage();
-
-        if (issuer.certificates[certificateID].isRevoked) {
-            revert ProofRevoked(certificateID);
-        }
-    }
-
     function checkProofExistance(uint256 certificateID) internal view {
         LibIssuer.IssuerStorage storage issuer = LibIssuer._getStorage();
 
+        // checks that certificat does exist
         if (certificateID > issuer.latestCertificateId) {
             revert NonExistingCertificate(certificateID);
         }
     }
 
     function checkProofRevocability(uint256 certificateID) internal view {
+        // checks that certificat does exist
+        checkProofExistance(certificateID);
+
         LibIssuer.IssuerStorage storage issuer = LibIssuer._getStorage();
 
+        // checks that certificate is not revokedProof
+        if (issuer.certificates[certificateID].isRevoked) {
+            revert ProofRevoked(certificateID);
+        }
+
         uint256 issuanceDate = issuer.certificates[certificateID].issuanceDate;
+
+        // checks revocable period
         if (issuanceDate + issuer.revocablePeriod < block.timestamp) {
             revert NonRevokableProof(certificateID, issuanceDate, issuanceDate + issuer.revocablePeriod);
+        }
+    }
+
+    function checkClaimableProof(uint256 certificateID, address claimer, uint256 claimedVolume, uint256 ownedBalance) internal view {
+        LibIssuer.IssuerStorage storage issuer = LibIssuer._getStorage();
+
+        // checks that certificate is not revokedProof
+        if (issuer.certificates[certificateID].isRevoked) {
+            revert ProofRevoked(certificateID);
+        }
+
+        // checks that the claimer owns enough volume
+        if (ownedBalance < claimedVolume) {
+            revert InsufficientBalance(claimer, certificateID, claimedVolume);
         }
     }
 
@@ -45,12 +62,6 @@ library LibProofManager {
     function checkProofValidity(bytes32 rootHash, bytes32 leaf, bytes32[] memory proof) internal pure {
         if (_verifyProof(rootHash, leaf, proof) == false) {
             revert InvalidProof(rootHash, leaf, proof);
-        }
-    }
-
-    function checkClaimedVolume(uint256 certificateID, address claimer, uint256 claimedVolume, uint256 ownedBalance) internal pure {
-        if (ownedBalance < claimedVolume) {
-            revert InsufficientBalance(claimer, certificateID, claimedVolume);
         }
     }
 
