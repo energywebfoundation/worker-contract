8fdfe7d5e68837cf5b80b04f2dc726286a2844f5
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2023-02-16 01:56:47 +0100

    feat(greenproof-contracts): adding approver role and approverFor feature

--- packages/greenproof-contracts/test/utils/roles.utils.js
+++ packages/greenproof-contracts/test/utils/roles.utils.js
@@ -4,12 +4,14 @@ const issuerRole = ethers.utils.namehash('minter.roles.greenproof.apps.iam.ewc')
 const revokerRole = ethers.utils.namehash('revoker.roles.greenproof.apps.iam.ewc');
 const workerRole = ethers.utils.namehash('workerRole.roles.greenproof.apps.iam.ewc');
 const claimerRole = ethers.utils.namehash('claimerRole.roles.greenproof.apps.iam.ewc');
+const approverRole = ethers.utils.namehash('approver.roles.greenproof.apps.iam.ewc');
 
 const roles = {
   issuerRole,
   revokerRole,
   workerRole,
   claimerRole,
+  approverRole,
 };
 
 module.exports = { roles };
