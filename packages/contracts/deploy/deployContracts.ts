c9ee3a64d4e517baa1998d9205e29d1edac3e950
Kevin Nzeng Essimengane <kevin.nzeng@energyweb.org>
2023-05-11 14:59:51 +0200

    chore(issuer.spec): fix testing and correctly use fixtures

--- packages/contracts/deploy/deployContracts.ts
+++ packages/contracts/deploy/deployContracts.ts
@@ -68,7 +68,9 @@ export const deployGreenproof = async (options: DeployGreeproofOptions) => {
     revokerRole = ethers.utils.namehash(process.env.REVOKER_ROLE ?? "revoker"),
     workerRole = ethers.utils.namehash(process.env.WORKER_ROLE ?? "worker"),
     claimerRole = ethers.utils.namehash(process.env.CLAIMER_ROLE ?? "claimer"),
-    approverRole = ethers.utils.namehash(process.env.APPROVER_ROLE ?? "approver"),
+    approverRole = ethers.utils.namehash(
+      process.env.APPROVER_ROLE ?? "approver"
+    ),
   } = roles;
 
   // deploy GreenproofInit
@@ -115,7 +117,11 @@ export const deployGreenproof = async (options: DeployGreeproofOptions) => {
   const metaCertificateInfos = ["SER Certificate", "SERC"];
 
   // call to init function
-  const functionCall = greeproofInit.interface.encodeFunctionData("init", [greenproof.address, certificateInfos, metaCertificateInfos]);
+  const functionCall = greeproofInit.interface.encodeFunctionData("init", [
+    greenproof.address,
+    certificateInfos,
+    metaCertificateInfos,
+  ]);
   const tx = await greenproof.diamondCut(
     cuts,
     greeproofInit.address,
